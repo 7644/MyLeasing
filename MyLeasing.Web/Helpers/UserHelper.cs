1816021072146e5324e7241e5a1385c0782248c1
7644 <smgarcia@integral.com.co>
2021-07-26 10:41:51 -0500

    edit owner

--- MyLeasing.Web/Helpers/UserHelper.cs
+++ MyLeasing.Web/Helpers/UserHelper.cs
@@ -95,5 +95,9 @@ namespace MyLeasing.Web.Helpers
             await _signInManager.SignOutAsync();
         }
 
+        public async Task<IdentityResult> UpdateUserAsync(User user)
+        {
+            return await _userManager.UpdateAsync(user);
+        }
     }
 }
